97eed57f31c66d67662f7169e33f99d62f4d79eb
GHawk1124 <garrett1124@icloud.com>
2019-11-21 10:54:26 -0700

    Testing Effectiveness of X bot Auton Code

--- 9573-X-DoorBot/src/main.cpp
+++ 9573-X-DoorBot/src/main.cpp
@@ -2,6 +2,35 @@
 #include "robotConfig.h"
 #include "userControl.h"
 
+static volatile double rfr;
+static volatile double rbr;
+static volatile double lfr;
+static volatile double lbr;
+
+void driveInchesGAR(double inches, int speed, int timeToSleep,
+                 vex::brakeType brakeType) {
+  driveTrain.setStopping(brakeType);
+  if (inches > 0) {
+    driveTrain.driveFor(FWD, inches, vex::distanceUnits::in, speed, vPCT, true);
+  } else {
+    driveTrain.driveFor(BWD, std::abs(inches), vex::distanceUnits::in, speed,
+                        vPCT, true);
+  }
+  vex::this_thread::sleep_for(timeToSleep);
+}
+
+void newDriveInchesGar(double inches, int speed, int timeToSleep,
+                 vex::brakeType brakeType) {
+	// Setup
+	{
+	vex::thread rfM (RF.rotation);
+	vex::thread rbM (RB.rotation);
+	vex::thread lfM (LF.rotation);
+	vex::thread lbM (RF.rotation);
+	}
+
+
+}
 
 void pre_auton() {
   RF.setStopping(MOTOR_STOPPING_DRIVE);
@@ -26,20 +55,37 @@ void pre_drive() {
 }
 
 void autonomous() {
-  driveInches(39);
-  driveInches(-15);
-  strafeInches(-6.5);
-  pointTurn(-90);
-  driveInches(7);
-  closeClaw();
-  driveInches(22);
-  pointTurn(-90);
-  driveInches(11);
-  // Not exact
-  //liftTo(180);
-  driveInches(2);
-  openClaw();
-  driveInches(-12);
+#ifdef LEFT_TURN
+  driveInchesGAR(35, 20, 500, BRAKE);
+  driveInchesGAR(-22.5, 40, 200, BRAKE);
+  Intake.spin(FWD, 10, vPCT);
+  driveTrain.turnFor(LEFT, 116, DEG);
+  Intake.stop();
+  vex::this_thread::sleep_for(200);
+  driveInchesGAR(13, 20, 200, BRAKE);
+  Intakef(BWD, 1250);
+  Liftf(FWD, 1000);
+  driveInchesGAR(-12, 20, 500, BRAKE);
+  Liftf(BWD, 1000);
+#endif
+#ifdef RIGHT_TURN
+  driveInchesGAR(40, 20, 500, BRAKE);
+  driveInchesGAR(-27.5, 40, 200, BRAKE);
+  Intake.spin(FWD, 10, vPCT);
+  driveTrain.turnFor(RIGHT, 119, DEG);
+  Intake.stop();
+  vex::this_thread::sleep_for(200);
+  driveInchesGAR(11, 20, 200, BRAKE);
+  Intakef(BWD, 1250);
+  Liftf(FWD, 1000);
+  driveInchesGAR(-12, 20, 500, BRAKE);
+  Liftf(BWD, 1000);
+#endif
+#ifdef SIMPLE
+  driveInchesGAR(12, 30, 300, BRAKE);
+  vex::this_thread::sleep_for(300);
+  driveInchesGAR(-10, 30, 300, BRAKE);
+#endif
 }
 
 void usercontrol() {
